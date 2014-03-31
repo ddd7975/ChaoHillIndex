21581af219c90639704b168fc4d1e01a6e0fdf90
ddd7975 <ddd7975@gmail.com>
2014-04-01 00:39:07 +0800

    Online Use

--- R/ChaoHill.R
+++ R/ChaoHill.R
@@ -17,14 +17,6 @@ function(dat, datatype=c("abundance", "incidence"), from=0, to=2, interval=0.1,
   }else{
     y <- dat[-1]; n <- dat[1]
   }
-  ###------------------------------------
-  conf.reg=function(x,LCL,UCL,...) {
-    x.sort <- order(x)
-    x <- x[x.sort]
-    LCL <- LCL[x.sort]
-    UCL <- UCL[x.sort]
-    polygon(c(x,rev(x)),c(LCL,rev(UCL)), ...)
-  }
   ###------------------------------------ 
   z <- -qnorm((1 - conf)/2)
   D <- length(which(y != 0)); U <- sum(y)
@@ -34,49 +26,38 @@ function(dat, datatype=c("abundance", "incidence"), from=0, to=2, interval=0.1,
   out.mle <- qDMLE(dat, q, B, datatype)
   ci.mle.l <- round(out.mle[1,] - z*out.mle[2,], 3)
   ci.mle.u <- round(out.mle[1,] + z*out.mle[2,], 3)
-  plot(q, out.pro[1,], type="l", lty=5, lwd=2, col=2, 
-       xlab="index q",
-       ylab=expression(paste(""^q, "D")), 
-       main = "Hill's Number",
-       ylim=c(min(ci.pro.l, ci.mle.l), max(ci.pro.u, ci.mle.u)))
-  points(q, out.mle[1,], type="l", lty=1, lwd=2)
-  
-  #  points(q, qdmle.s, type = "l", lty = 3, lwd = 2, col = 4)
-  conf.reg(q, ci.mle.l, ci.mle.u, col=adjustcolor("red", 0.25), border=NA)
-  #  points(q, qdpro.s, type = "l", lty = 5, lwd = 2, col = 2)
-  conf.reg(q, ci.pro.l, ci.pro.u, col=adjustcolor("green", 0.25), border=NA)
-  legend("topright", legend=c("MLE", "Proposed"),
-         lty=c(1, 5), lwd=c(1, 2), col=c(1, "red"), cex=1, bty="n")
+#   plot(q, out.pro[1,], type="l", lty=5, lwd=2, col=2, 
+#        xlab="index q",
+#        ylab=expression(paste(""^q, "D")), 
+#        main = "Hill's Number",
+#        ylim=c(min(ci.pro.l, ci.mle.l), max(ci.pro.u, ci.mle.u)))
+#   points(q, out.mle[1,], type="l", lty=1, lwd=2)
+#   
+#   #  points(q, qdmle.s, type = "l", lty = 3, lwd = 2, col = 4)
+#   conf.reg(q, ci.mle.l, ci.mle.u, col=adjustcolor("red", 0.25), border=NA)
+#   #  points(q, qdpro.s, type = "l", lty = 5, lwd = 2, col = 2)
+#   conf.reg(q, ci.pro.l, ci.pro.u, col=adjustcolor("green", 0.25), border=NA)
+#   legend("topright", legend=c("MLE", "Proposed"),
+#          lty=c(1, 5), lwd=c(1, 2), col=c(1, "red"), cex=1, bty="n")
   if (detail == TRUE){
     table.est <- data.frame(matrix(c(out.mle[1,], out.pro[1,]),
                                    nrow=2, byrow=T))
+    table.sd <- data.frame(matrix(c(out.mle[2,], out.pro[2,]),
+                                  nrow=2, byrow=T))
+    table.lci <- table.est - z*table.sd
+    table.uci <- table.est + z*table.sd
+    colnames(table.est) <- colnames(table.sd) <- colnames(table.lci) <- colnames(table.uci) <- paste("q=", q, sep="")    
   }else{
-    table.est <- data.frame(matrix(c(out.mle[1,][which(round(q)==q)], out.pro[1,][which(round(q)==q)]),
-                                   nrow=2, byrow=T))
     table.est <- data.frame(matrix(c(out.mle[1,][which((q %in% c(0, 1, 2))==T)],
                                      out.pro[1,][which((q %in% c(0, 1, 2))==T)]),
                                    nrow=2, byrow=T))
-  }
-  rownames(table.est)=c("Obersved", "Chao_2013")
-  if (detail==T){
-    colnames(table.est)=paste("q=", q, sep="")    
-  }else{
-    colnames(table.est)=c("q=0", "q=1", "q=2")
-  }
-  
-  if(detail==T){
-    table.sd <- data.frame(matrix(c(out.mle[2,], out.pro[2,]),
-                                  nrow=2, byrow=T))
-  }else{
     table.sd <- data.frame(matrix(c(out.mle[2,][which((q %in% c(0, 1, 2))==T)], 
                                     out.pro[2,][which((q %in% c(0, 1, 2))==T)]),
-                                nrow=2, byrow=T))
+                                  nrow=2, byrow=T))
+    table.lci <- table.est - z*table.sd
+    table.uci <- table.est + z*table.sd
+    colnames(table.est) <- colnames(table.sd) <- colnames(table.lci) <- colnames(table.uci) <- c("q=0", "q=1", "q=2")
   }
-  rownames(table.sd)=c("Obsersved", "Chao_2013")
-  if(detail==T){
-    colnames(table.sd)=paste("q=", q, sep="")  
-    }else{
-    colnames(table.sd)=c("q=0", "q=1", "q=2")
-  }
-  return(list(EST = table.est, SD = table.sd))
+  rownames(table.est) <- rownames(table.sd) <- rownames(table.lci) <- rownames(table.uci) <- c("Obersved", "Chao_2013")
+  return(list(EST = table.est, SD = table.sd, LCI = table.lci, UCI = table.uci))
 }
